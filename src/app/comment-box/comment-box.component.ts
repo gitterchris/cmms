1c145798eaf6dba2221fc937298eaeb3ffb4681f
Christopher Ababan <chris@ababan.us>
2024-04-21 23:59:53 -0400

    Add basic user list filtering capability

--- src/app/comment-box/comment-box.component.ts
+++ src/app/comment-box/comment-box.component.ts
@@ -15,18 +15,53 @@ import { User } from '../types';
 export class CommentBoxComponent {
   @Input() form!: FormGroup;
   users!: User[];
+  filteredUsers!: User[];
   showUserList = false;
+  /* 
+    We need the '@' as the first element to determine
+    if the popup will still be shown after repeated backspace. 
+  */
+  filter: string[] = ['@'];
 
   constructor(private userService: UsersService) {}
 
   ngOnInit(): void {
     this.users = this.userService.getUsers();
+    this.filteredUsers = [...this.users];
   }
 
   onKeydown(event: KeyboardEvent) {
+    console.log('CHRIS!!! KEY ENTERED!!!', event);
     if (event.key === '@') {
       this.showUserList = true;
     }
+    if (event.key === ' ') {
+      this.resetUserList();
+    }
+
+    if (this.showUserList) {
+      if (this.isLetter(event)) {
+        this.filter.push(event.key);
+      } else if (event.key === 'Backspace') {
+        this.filter.pop();
+        if (!this.filter.length) {
+          this.resetUserList();
+          return;
+        }
+      }
+      this.filteredUsers = this.userService.filterUsers(
+        this.filter.join('').substring(1)
+      );
+    }
+  }
+
+  resetUserList() {
+    this.showUserList = false;
+    this.filter = ['@'];
+  }
+
+  isLetter(event: KeyboardEvent) {
+    return event.which >= 65 && event.which <= 90;
   }
 
   handleUserHover(userID: number) {
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -7,12 +7,21 @@ export class UsersService {
   constructor() {}
 
   getUsers() {
-    // TODO: retrieve this from http.get();
+    // TODO: retrieve this from http.get() and cache the result;
     return [
       { userID: 1, name: 'Kevin' },
       { userID: 2, name: 'Jeff' },
       { userID: 3, name: 'Bryan' },
       { userID: 4, name: 'Gabbey' },
     ];
+  }
+
+  filterUsers(filter: string) {
+    const allUsers = this.getUsers();
+    if (!filter) return allUsers;
+
+    return allUsers.filter((user) =>
+      user.name.toLowerCase().startsWith(filter.toLowerCase())
+    );
   }
 }
